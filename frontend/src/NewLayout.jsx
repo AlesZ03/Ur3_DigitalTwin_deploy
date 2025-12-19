f5235ed22b83e06ea9ff1ad7804c475583c130aa
Balázs Vajk-Istiván <balazsvajk2003@gmail.com>
2025-12-19 16:28:47 +0200

    teszt3

--- frontend/src/NewLayout.jsx
+++ frontend/src/NewLayout.jsx
@@ -157,10 +157,10 @@ function RobotModel({ jointData, ...props }) {
               joint.rotation.z = THREE.MathUtils.lerp(joint.rotation.z, value, 0.1);
               break;
             case 4: // Wrist02
-              joint.rotation.z = THREE.MathUtils.lerp(joint.rotation.y, value, 0.1);
+              joint.rotation.x = THREE.MathUtils.lerp(joint.rotation.y, value, 0.1);
               break;
             case 5: // Wrist03
-              joint.rotation.z = THREE.MathUtils.lerp(joint.rotation.z, value, 0.1);
+              joint.rotation.y = THREE.MathUtils.lerp(joint.rotation.z, value, 0.1);
               break;
             default:
               break;
