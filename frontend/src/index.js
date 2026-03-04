04537f469cc85b20c4fe5ba40bb413f7f871d22a
Balázs Vajk-Istiván <balazsvajk2003@gmail.com>
2026-03-04 11:57:01 +0200

    modified:   frontend/src/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Configure Amplify
+const amplifyConfig = {
+  aws_appsync_graphqlEndpoint: process.env.REACT_APP_APPSYNC_URL,
+  aws_appsync_region: process.env.REACT_APP_APPSYNC_REGION,
+  aws_appsync_authenticationType: 'API_KEY',
+  aws_appsync_apiKey: process.env.REACT_APP_APPSYNC_API_KEY,
+};
+
+Amplify.configure(amplifyConfig);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
